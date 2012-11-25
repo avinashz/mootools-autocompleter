636209d390cf7825c74ef9445871af89e7ccb82a
Avinash <info@xpertdeveloper.com>
2012-11-25 17:46:05 +0530

    Example Added

---
+++ example/ajax.php
@@ -0,0 +1,5 @@
+<?php
+// This is the example only but here you can do what ever server side scripting here and prepare your output data.
+	$names = array('First','Second','Third','Fourth');
+	echo json_encode($names); die;
+?>
